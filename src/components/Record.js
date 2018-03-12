e66001adce4f72e7f568281573f6980116364fe7
wb-ghb226150 <wb-ghb226150@alibaba-inc.com>
2018-03-12 22:40:34 +0800

    first

---
+++ src/components/Record.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+
+
+class Records extends Component {
+    render() {
+        return (
+                <tr>
+                    <td>2019-01-09</td>
+                    <td>收入</td>
+                    <td>11111</td>
+                </tr>
+        );
+    }
+}
+
+export default Records;
---
+++ src/components/Records.js
@@ -0,0 +1,35 @@
+import React, {Component} from 'react';
+import Record from './Record'
+
+class Records extends Component {
+    constructor(){
+        super();
+        this.state={
+            records:[
+                {'id':1,'date':'2019-01-09','title':'收入','amount':20},
+                {'id':2,'date':'2019-01-09','title':'奖金','amount':45}
+            ]
+        }
+    }
+    render() {
+        return (
+            <div>
+                <h2>Records</h2>
+                <table className="table table-bordered">
+                    <thead>
+                        <tr>
+                            <th>Date</th>
+                            <th>Title</th>
+                            <th>Amount</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {this.state.records.map((record,i)=><Record />)}
+                    </tbody>
+                </table>
+            </div>
+        );
+    }
+}
+
+export default Records;
