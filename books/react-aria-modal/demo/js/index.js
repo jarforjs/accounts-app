8636535658498ce553a67061df64e2978c4fadcb
顾洪波 <wb-ghb226150@alibaba-inc.com>
2018-07-04 11:31:34 +0800

    add aria-modal

---
+++ books/react-aria-modal/demo/js/index.js
@@ -0,0 +1,10 @@
+require('./demo-one');
+require('./demo-two');
+require('./demo-three');
+require('./demo-four');
+require('./demo-five');
+require('./demo-six');
+require('./demo-seven');
+require('./demo-eight');
+require('./demo-nine');
+require('./demo-ten');
