4c45f3fb56f928de190e729e17c1fc12e10a69a8
jarforjs <114571257@qq.com>
2018-07-23 23:20:42 +0800

    add

--- books/eventEmitter1.js
+++ books/eventEmitter1.js
@@ -150,16 +150,10 @@ import { event } from "./EmitterEvent";
 //     }
 // }
 
-<<<<<<< HEAD
 
-class EventEmitter{
-    constructor(){
-        this._events={}
-=======
 class EventEmitter {
     constructor() {
         this._events = {}
->>>>>>> 071bc735c41b7b08a85333afc75b3a2c398c3be1
     }
 
     on(eventName,handler){
@@ -181,34 +175,15 @@ class EventEmitter {
     once(eventName,handler){
         const that=this;
         function func() {
-<<<<<<< HEAD
-            const args=Array.prototype.slice.call(arguments,0)
-            handler.apply(that,args)
-            this.off(eventName,func)
-=======
+
             const args = Array.prototype.slice.call(arguments, 0);
             handler.apply(that, args);
             this.off(eventName, func);
->>>>>>> 071bc735c41b7b08a85333afc75b3a2c398c3be1
         }
         this.on(eventName,func)
     }
 
-<<<<<<< HEAD
-    emit(eventName){
-        let events=this._events[eventName];
-        const args=Array.prototype.slice.call(arguments,1);
-        const that =this;
-        if(events&&events.length){
-            events.forEach(event=>event.apply(that,args))
-        }
-    }
 
-    emit(eventName,...args){
-        const events=this._events[eventName];
-        if(events&&events.length){
-            for(const event of events){
-=======
     emit(eventName) {
         const events = this._events[eventName];
         const args = Array.prototype.slice.call(arguments, 1);
@@ -222,7 +197,6 @@ class EventEmitter {
         const events = this._events[eventName];
         if (events && events.length) {
             for (const event of events) {
->>>>>>> 071bc735c41b7b08a85333afc75b3a2c398c3be1
                 event(...args)
             }
         }
